b4c72dd7fd42ac5cc71acd8dec9f19d15d22e9e2
Kate0418 <nakao.keito0418@gmail.com>
2026-03-13 14:31:41 +0900

    env

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -3,17 +3,29 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+  ->withRouting(
+    web: __DIR__ . '/../routes/web.php',
+    api: __DIR__ . '/../routes/api.php',
+    commands: __DIR__ . '/../routes/console.php',
+    health: '/up',
+  )
+  ->withMiddleware(function (Middleware $middleware): void {
+    //
+  })
+  ->withExceptions(function (Exceptions $exceptions): void {
+    $exceptions->render(function (ValidationException $e) {
+      $messages = collect($e->errors())
+        ->map(function ($messages) {
+          return $messages[0];
+        })
+        ->toArray();
+
+      return response()->json([
+        "type" => "Validation",
+        "fieldErrors" => $messages
+      ], 422);
+    });
+  })->create();
